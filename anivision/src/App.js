af6bea3d2bedab90d7b53b001532514df557db19
Shiyao <swan485@aucklanduni.ac.nz>
2020-08-17 02:42:42 +1200

    created react app

---
+++ anivision/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import logo from "./logo.svg";
+// import topNav from "./components/topNav";
+import "./App.css";
+
+function App() {
+  return (
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      Button
+    </button>
+  );
+}
+
+export default App;
