b76a21fb088948fd96fa1f16256fc465bba98f14
unknown <swangwang2000@gmail.com>
2021-08-01 04:20:30 +1200

    Updates

---
+++ precache-manifest.b04cddc4da88aa6c51f3921a88432f04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263f2c0a256a079840cf1d91b298e6e0",
+    "url": "/anivision/index.html"
+  },
+  {
+    "revision": "b32da3f6327906195c0a",
+    "url": "/anivision/static/css/main.be7d3e6f.chunk.css"
+  },
+  {
+    "revision": "db4a0685cb7c8abfef8b",
+    "url": "/anivision/static/js/2.2b8fdb68.chunk.js"
+  },
+  {
+    "revision": "ccd0465d4ce8def75c971e064a048e5b",
+    "url": "/anivision/static/js/2.2b8fdb68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32da3f6327906195c0a",
+    "url": "/anivision/static/js/main.1f5538a9.chunk.js"
+  },
+  {
+    "revision": "ba65e0698ce6fcc58f74",
+    "url": "/anivision/static/js/runtime-main.edda786f.js"
+  }
+]);
