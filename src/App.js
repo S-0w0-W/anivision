43fd0c5dea38e530776bfc78a2b33eeb32cdd6f0
Shiyao-Wang <shiyao.wang@asbuiltdigital.com>
2021-08-03 12:01:19 +1200

    Route changed again
    pls work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./App.css";
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from "react-router-dom";
 
 import Main from "./pages/index";
@@ -12,9 +13,33 @@ import Three from "./pages/three";
 function App() {
   return (
     <Router>
-      <Route exact path="anivision/Home" component={Main} />
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+
+            </li>
+
+            <li>
+
+            </li>
+
+            <li>
+
+            </li>
+          </ul>
+
+        </nav> */}
+
+        <Switch>
+          <Route path="/home" component={Main} />
+          <Route path="/about" component={About} />
+          <Route path="/three" component={Three} />
+        </Switch>
+      </div>
+      {/* <Route exact path="anivision/Home" component={Main} />
       <Route exact path="anivision/About" component={About} />
-      <Route exact path="anivision/Three" component={Three} />
+      <Route exact path="anivision/Three" component={Three} /> */}
     </Router>
   );
 }
